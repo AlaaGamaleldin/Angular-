42ca23a6d1af42f9b6267d2ee3bd88896e5178e5
Alaa Mohammed Gamal Eldin <alaadesigner85@gmail.com>
2024-02-14 20:05:58 +0200

    Routing

---
+++ Router.Manoj/start/src/app/courses/course-detail/course-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Course } from 'src/app/Models/course';
+import { CourseService } from 'src/app/Services/course.service';
+
+@Component({
+  selector: 'app-course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrls: ['./course-detail.component.css']
+})
+export class CourseDetailComponent implements OnInit{
+selectedCourse: Course;
+courseId: number;
+
+courseSerice: CourseService = inject(CourseService);
+activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+ngOnInit(){
+//this.courseId = this.activeRoute.snapshot.params['id'];
+this.courseId = +this.activeRoute.snapshot.paramMap.get['id'];
+console.log(this.courseId);
+}
+}
