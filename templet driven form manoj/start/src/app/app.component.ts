53cbe72dede88736cc193ccf25816ac498ba91c0
Alaa Mohammed Gamal Eldin <alaagamalel-din@hotmail.com>
2024-03-08 11:36:42 +0200

    ngForm and touched

--- templet driven form manoj/start/src/app/app.component.ts
+++ templet driven form manoj/start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-form';
+
+  @ViewChild('registrationForm') form: NgForm;
+
+  OnFormSubmited(){
+    console.log(this.form);
+    console.log(this.form.touched);
+    console.log(this.form.controls['firstname'].value);
+  }
 }
