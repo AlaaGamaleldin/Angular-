b2454cf26e78a8578d15ac50d80177b0c6185109
Alaa Mohammed Gamal Eldin <alaagamalel-din@hotmail.com>
2024-03-10 21:23:11 +0200

    genetate username using setValue() and patchValue()

--- templet driven form manoj/start/src/app/app.component.ts
+++ templet driven form manoj/start/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   firstName: string = '';
   lastName: string = '';
+  dob: string = '';
   emailAddress: string = '';
 
 
@@ -27,5 +28,48 @@ export class AppComponent {
     console.log(this.form);
     console.log(this.form.touched);
     console.log(this.form.controls['firstname'].value);
+  }
+  GenerateUsername(){
+    let username = '';
+
+    if(this.firstName.length >= 3){
+      username += this.firstName.slice(0, 3);
+    }else{
+      username += this.firstName;
+    }
+    if(this.lastName.length >= 3){
+      username += this.lastName.slice(0, 3);
+    }else{
+      username += this.lastName;
+    }
+
+    let datetime = new Date(this.dob);
+    username += datetime.getFullYear();
+
+    username = username.toLowerCase();
+    console.log(username);
+
+    
+
+    // this.form.setValue({
+    //   firstname: this.form.value.firstname,
+    //   lastname: this.form.value.lastname,
+    //   email: this.form.value.email,
+    //   phone: this.form.value.phone,
+    //   gender: this.form.value.gender,
+    //   dob: this.form.value.dob,
+    //   username: username,
+    //   address: {
+    //     street1: this.form.value.address.street1,
+    //     street2: this.form.value.address.street2,
+    //     country: this.form.value.address.country,
+    //     city: this.form.value.address.city,
+    //     region: this.form.value.address.region,
+    //     postal: this.form.value.address.postal,
+    //   }
+    // });
+    this.form.form.patchValue({
+      username: username
+    })
   }
 }
