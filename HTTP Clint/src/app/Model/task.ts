8e1273034b9c6ac3ba8f3376db1e5f5303ea55d8
Alaa Mohammed Gamal Eldin <alaagamalel-din@hotmail.com>
2024-03-16 20:09:40 +0200

    http request with post and get

--- HTTP Clint/src/app/Model/task.ts
+++ HTTP Clint/src/app/Model/task.ts
@@ -2,10 +2,11 @@ export class Task{
     constructor(
         public title: string,
         public desc: string,
-        public assigedTO: string,
+        public assignedTo: string,
         public createdAt: string,
         public priority: string,
-        public status: string
+        public status: string,
+        public id?: string
         ){
 
     }
--- HTTP Clint/src/app/dashboard/dashboard.component.ts
+++ HTTP Clint/src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { Task } from '../Model/task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent{
+export class DashboardComponent implements OnInit{
   showCreateTaskForm: boolean = false;
+  http: HttpClient = inject(HttpClient);
+  allTasks: Task[] = [];
+
+  ngOnInit(){
+    this.fetchAllTasks();
+  }
 
   OpenCreateTaskForm(){
     this.showCreateTaskForm = true;
@@ -18,6 +26,30 @@ export class DashboardComponent{
 
   }
   CreateTask(data: Task){
-    console.log( data)
+    const headers = new HttpHeaders({'My-header': 'hellow-world'})
+    this.http.post<{name: string}>('https://angularhttpclint-6c95e-default-rtdb.firebaseio.com/tasks.json', data, {headers: headers})
+    .subscribe((response) => {
+      console.log(response);
+      //this.fetchAllTasks();
+    })
+  }
+  FetchAllTasks(){
+    this.fetchAllTasks();
+  }
+  private fetchAllTasks(){
+    this.http.get<{[key: string]: Task}>('https://angularhttpclint-6c95e-default-rtdb.firebaseio.com/tasks.json')
+    .pipe(map((response) => {
+      //transforming the data form obj to Array
+      let tasks = [];
+      for(let key in response){
+        if(response.hasOwnProperty(key)){
+          tasks.push({...response[key], id: key});
+        }
+      }
+      return tasks;
+    }))
+    .subscribe((tasks) => {
+      this.allTasks = tasks;
+    })
   }
 }
